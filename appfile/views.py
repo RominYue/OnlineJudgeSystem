4ca98deacdf7b79b369e4c4c6361df14ae9eec0c
yomin <yueming412@gmail.com>
2014-11-06 20:59:40 +0800

    add base.html,style.css,route for '/','/login','/register'

--- appfile/views.py
+++ appfile/views.py
@@ -6,5 +6,14 @@ from appfile import app
 import flask
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'hello world!'
+    return flask.render_template('index.html')
+
+@app.route('/login')
+def login():
+    return flask.render_template('login.html')
+
+@app.route('/register')
+def register():
+    return flask.render_template('register.html')
