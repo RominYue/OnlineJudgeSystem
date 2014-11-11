36a203104c80b63301ae21867cdaad4483788b41
yomin <yueming412@gmail.com>
2014-11-11 19:45:15 +0800

    show part problem.html, but have bugs when route for '/problemset/page=2',no css results

--- appfile/__init__.py
+++ appfile/__init__.py
@@ -16,9 +16,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = '/login'
 
-from appfile import views
-from models import User
-
 print os.path.abspath(sys.path[0])
 
 
